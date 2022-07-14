e744756375a48b77faad4141005808b2913c0207
Piero Toffanin <pt@masseranolabs.com>
2022-07-14 16:03:51 +0000

    Don't use Python function names

--- guidedfilter.py
+++ guidedfilter.py
@@ -1,37 +1,35 @@
 import numpy as np
 
 def box(img, radius):
-
-    (rows, cols) = img.shape[:2]
     dst = np.zeros_like(img)
+    (r, c) = img.shape[:2]
 
-    seg = [1] * img.ndim
-    seg[0] = radius
-    sum = np.cumsum(img, 0)
-    dst[0:radius+1, :, ...] = sum[radius:2*radius+1, :, ...]
-    dst[radius+1:rows-radius, :, ...] = sum[2*radius+1:rows, :, ...] - sum[0:rows-2*radius-1, :, ...]
-    dst[rows-radius:rows, :, ...] = np.tile(sum[rows-1:rows, :, ...], seg) - sum[rows-2*radius-1:rows-radius-1, :, ...]
+    s = [1] * img.ndim
+    s[0] = radius
+    c_sum = np.cumsum(img, 0)
+    dst[0:radius+1, :, ...] = c_sum[radius:2*radius+1, :, ...]
+    dst[radius+1:r-radius, :, ...] = c_sum[2*radius+1:r, :, ...] - c_sum[0:r-2*radius-1, :, ...]
+    dst[r-radius:r, :, ...] = np.tile(c_sum[r-1:r, :, ...], s) - c_sum[r-2*radius-1:r-radius-1, :, ...]
 
-    seg = [1] * img.ndim
-    seg[1] = radius
-    sum = np.cumsum(dst, 1)
-    dst[:, 0:radius+1, ...] = sum[:, radius:2*radius+1, ...]
-    dst[:, radius+1:cols-radius, ...] = sum[:, 2*radius+1 : cols, ...] - sum[:, 0 : cols-2*radius-1, ...]
-    dst[:, cols-radius: cols, ...] = np.tile(sum[:, cols-1:cols, ...], seg) - sum[:, cols-2*radius-1 : cols-radius-1, ...]
+    s = [1] * img.ndim
+    s[1] = radius
+    c_sum = np.cumsum(dst, 1)
+    dst[:, 0:radius+1, ...] = c_sum[:, radius:2*radius+1, ...]
+    dst[:, radius+1:c-radius, ...] = c_sum[:, 2*radius+1 : c, ...] - c_sum[:, 0 : c-2*radius-1, ...]
+    dst[:, c-radius: c, ...] = np.tile(c_sum[:, c-1:c, ...], s) - c_sum[:, c-2*radius-1 : c-radius-1, ...]
 
     return dst
 
 
-def guided_filter(input, filter, radius, eps):
+def guided_filter(img, guide, radius, eps):
+    (r, c) = img.shape
 
-    (rows, cols) = input.shape
+    CNT = box(np.ones([r, c]), radius)
 
-    CNT = box(np.ones([rows, cols]), radius)
+    mean_img = box(img, radius) / CNT
+    mean_guide = box(guide, radius) / CNT
 
-    mean_input = box(input, radius) / CNT
-    mean_filter = box(filter, radius) / CNT
+    a = ((box(img * guide, radius) / CNT) - mean_img * mean_guide) / (((box(img * img, radius) / CNT) - mean_img * mean_img) + eps)
+    b = mean_guide - a * mean_img
 
-    a = ((box(input * filter, radius) / CNT) - mean_input * mean_filter) / (((box(input * input, radius) / CNT) - mean_input * mean_input) + eps)
-    b = mean_filter - a * mean_input
-
-    return (box(a, radius) / CNT) * input + (box(b, radius) / CNT)
+    return (box(a, radius) / CNT) * img + (box(b, radius) / CNT)
